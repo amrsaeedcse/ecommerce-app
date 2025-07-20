739b2b94e3eb02e02e2f25bdd2daa617d750999d
amrsaeedcse <amrabdelazeem117@gmail.com>
2025-07-21 00:29:53 +0300

    just edited some in forget screen

--- lib/screens/forgetpass.dart
+++ lib/screens/forgetpass.dart
@@ -21,6 +21,7 @@ class ForgetPass extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ////// here test git hub
     final theme = Theme.of(context);
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
